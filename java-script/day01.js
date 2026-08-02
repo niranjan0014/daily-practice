2ac02d1799dcdc85f79b2071f38b757c6b392d0f
niranjan0014 <niranjanmahanta2004@gmail.com>
2026-08-02 20:23:30 +0530

    hi

--- java-script/day01.js
+++ java-script/day01.js
@@ -1,5 +1,20 @@
 //array
-var arr=[1,2,3,4];
-console.log(arr);
+// var arr=[1,2,3,4];
+// console.log(arr);
 
-console.log("hello world"); 
+// console.log("hello world"); 
+//function
+function sub(a,c){
+    return a-c;
+}
+var r=sub(11,1);
+console.log(r);
+var car={
+};
+car.model="audi";
+car.color="blue";
+console.log(car);
+car.fn=function() {
+    return a+b;
+}
+var 
